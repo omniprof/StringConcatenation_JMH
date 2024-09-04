4e8d35fba456ba149bcd445be641fff6ed21f27c
Ken Fogel <omniprof@users.noreply.github.com>
2024-09-04 23:33:55 +0200

    Update to concatenation

--- src/main/java/com/kenfogel/MyBenchmark.java
+++ src/main/java/com/kenfogel/MyBenchmark.java
@@ -17,23 +17,23 @@ public class MyBenchmark {
     @Fork(value = 1)
     @Measurement(iterations = 2, time = 2)
     @Warmup(iterations = 2, time = 2)
-    public void stringConcat() {
+    public String stringConcat() {
         String result = "";
         for (int i = 0; i < 1000; i++) {
             result += String.valueOf(i);
         }
-        //return result;
+        return result;
     }
 
     @Benchmark
     @Fork(value = 1)
     @Measurement(iterations = 2, time = 2)
     @Warmup(iterations = 2, time = 2)
-    public void stringBuilderConcat() {
+    public String stringBuilderConcat() {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < 1000; i++) {
             result.append(i);
         }
-        //return result.toString();
+        return result.toString();
     }
 }
